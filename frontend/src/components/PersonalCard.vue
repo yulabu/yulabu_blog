95dca0337c8d156160ec1099976f8d8f78ffccca
!spicy_fish? <1458661453@qq.com>
2026-07-18 21:31:06 +0800

    个人卡片组件样式修改

--- frontend/src/components/PersonalCard.vue
+++ frontend/src/components/PersonalCard.vue
@@ -1,23 +1,123 @@
 <template>
   <div class="card">
-    <div class="container"></div>
-    <img src="../../assets/img/Personal_img.jpg" alt="">
-    <h3 class="name">Yulabu</h3>
-    <p class="say">记录学习，分享生活。</p>
-    <div class="socials">
-      <span class="social-icon">🐙</span>
-      <span class="social-icon">📧</span>
-      <!-- 以后换成真实链接：<a href="...">图标</a> -->
+    <div class="card-top">
+      <div class="image"></div>
+    </div>
+    <div class="card-bottom">
+      <h2 class="name">Yulabu</h2>
+      <span>记录学习，分享生活。</span>
+    </div>
+    <div class="card-bottom2">
+      <ul>
+        <li>QQ:1458661453</li>
+        <li>WX:yulabu_nxd</li>
+        <li>GitHub:yulabu</li>
+      </ul>
     </div>
   </div>
 </template>
 <style scoped>
 /* 调试暂用大小，后面记得改成相对 */
 .card{
-    background-color: aquamarine;
+  position: relative;
+  width: 300px;
+  height: 260px;
+  background-color: rgb(234, 248, 236);
+  box-shadow: 0 0 10px rgba(0,0,0,0.5);
+  overflow: hidden;
+  border-radius: 0 0 30px 30px;
 }
-img{
-    width: 200px;
-    height: 200px;
+.card-top{
+  position: relative;
+  width: 330px;
+  height: 130px;
+  background-color: rgb(167, 233, 186);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  transition:  .5s;
+}
+/* 裁剪小三角 */
+.card-top::after{
+  content: '';
+  position: absolute;
+  width: 40px;
+  height: 40px;
+  background-color: rgb(167, 233, 186);
+  transition: translate(-50%);
+  /* left: 50%; */
+  bottom: -30%;
+  clip-path: polygon(50% 40%,0 0,100% 0);
+  z-index: 9;
+}
+.image{
+  position: absolute;
+  top: 20px;
+  width: 90px;
+  height: 90px;
+  background-image: url('../../assets/img/Personal_img.jpg');
+  background-size: cover;
+  border: 5px solid rgb(139, 194, 155);;
+  transition: .5s;
+}
+.card-bottom, .card-bottom2{
+  position: absolute;
+  top: 150px;
+  width: 330px;
+  height: 130px;
+  text-align: center;
+  transition: .5s;
+}
+.card-bottom h2{
+  display: block;
+  margin: 10px 0 10px 0;
+  font: 800 28px "Hobo Std";
+}
+.card-bottom span{
+  /* display: block; */
+  font-size: 400 24px 'Giddyup Std';
+  color: rgb(100, 100, 100);
+}
+.card-bottom h2::after{
+  content: "";
+  display: block;
+  width: 200px;
+  height: 2px;
+  background-color: black;
+  position: absolute;
+  left: 50%;
+  transform: translate(-50%, 4px);
+}
+.card-bottom2{
+  top: 300px;
+  height: 30%;
+}
+.card-bottom2 li{
+  margin: 26px;
+  font: 800 24px 'OCR A Std';
+  transition: .2s;
+}
+ul{
+  list-style: none;
+}
+/* 鼠标移入动画实现 */
+.card:hover .card-top{
+  height: 50px;
+}
+.card:hover .image{
+  width: 35px;
+  height: 35px;
+  border-width: 3px;
+  transform: translate(-140px,-15px);
+}
+.card:hover .card-bottom{
+  top: 300px;
+}
+.card:hover .card-bottom2{
+  top: 50px;
+}
+li:hover{
+  color: rgb(87, 228, 191);
+  transform: translateY(-10px);
 }
 </style>
--- frontend/src/components/PostList.vue
+++ frontend/src/components/PostList.vue
@@ -5,41 +5,37 @@
       <div class="cover"></div>
       <div class="info">
         <h4>{{ post.title }}</h4>
-        <p class="excerpt">{{ post.excerpt }}</p>
+        <p class="excerpt">{{ post.summary }}</p>
         <div class="meta">
-          <span class="date">{{ post.date }}</span>
-          <span v-for="tag in post.tags" :key="tag" class="tag">{{ tag }}</span>
+          <span class="date">{{ post.createdAt }}</span>
+          <span v-for="tag in post.tags" :key="tag" class="tag">{{ post.category.name }}</span>
         </div>
       </div>
     </article>
   </section>
 </template>
 <script setup>
-const posts = [
-  {
-    id: 1,
-    title: '开始搭建我的个人博客',
-    excerpt: '从零开始，使用 Vue3 + Vite 一步步搭建属于自己的博客站点...',
-    date: '2026-06-20',
-    tags: ['Vue3', '博客']
-  },
-  {
-    id: 2,
-    title: '登录页面的设计思路',
-    excerpt: '记录了登录页从设计稿到实现的完整过程，包括玻璃拟态和动画效果...',
-    date: '2026-06-22',
-    tags: ['CSS', '设计']
-  },
-  {
-    id: 3,
-    title: 'Vue Router 路由配置笔记',
-    excerpt: '整理了 Vue Router 的基本用法、路由守卫和页面懒加载的实现方式...',
-    date: '2026-06-23',
-    tags: ['Vue', 'Router']
+import { ref, onMounted } from 'vue'
+
+const posts = ref([])
+
+onMounted(async () => {
+  try {
+    const res = await fetch('/api/posts')
+    const data = await res.json()
+    posts.value = data.posts
+  } catch (e) {
+    console.error('获取文章列表失败:', e)
   }
-]
+})
 </script>
 <style scoped>
+.post-list {
+  display: flex;
+  flex-direction: column;
+  gap: 1rem;
+  padding: 1rem;
+}
 .post-list{
     background-color: bisque;
 }
